0bca016d48f753162668704eace674f37830f977
11BelowStudio <r_m_lowe@outlook.com>
2021-02-17 17:34:23 +0000

    welp, that's lab 4 done, I guess. also got some pointers for the assignment, those have been left as TODOs for now.

--- assignment/ce316ass.py
+++ assignment/ce316ass.py
@@ -19,5 +19,20 @@ for frame in range (0, nframes):
     im_right = cv2.imread(fn_right)
     print(fn_left)
     print(fn_right)
+    # TODO
+    # we know background is exactly black
+    # each object is a different colour
+    # if spot the red one in the left,
+    #   know that the red one in the right is the same
+    #   look for leftmost, rightmost, highest,
+    #   and lowest pixel that is red?
+    # make each pixel above a certain value into a region,
+    #   see what regions match?
+    # can threshold a colour image, but most stuff so far is for greyscale
+    #   make it greyscale
+    #   find the regions from the greyscale image
+    #   look at those regions in the colour image
+    #   identify the colour
+    #   ta-daa
 
 print("sugma")
